0871d01c349438947db1344553ee2355b9ef585b
rakutore <tomosan100@yahoo.co.jp>
2026-03-18 11:27:52 +0900

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,10 @@ async function issueDownloadToken(email) {
 // 管理画面ベーシック認証
 // ================================
 function basicAuth(req, res, next) {
-  const auth = { login: 'rakutore-admin', password: 'RakuAnchor!2026' }
+  const auth = {
+  login: process.env.ADMIN_USER,
+  password: process.env.ADMIN_PASS
+}
 
   const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
   const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
